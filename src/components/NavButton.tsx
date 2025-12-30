ea46fc575d570503e21a4f60ca7724451b5ab9a2
Zayar Shein <alexleotwd@gmail.com>
2025-12-30 23:36:54 +0800

    Add react-icons dependency and implement responsive navigation menu for mobile view

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -2,10 +2,11 @@ import type { ReactNode } from "react"
 import { Link } from "react-scroll";
 interface NavButtonProps {
     children: ReactNode
+    onClick?: () => void
 }
-// use Link instead of a tag for client side routing
-export default function NavButton({children}: NavButtonProps) {
+
+export default function NavButton({children, onClick}: NavButtonProps) {
     return (
-        <Link to={children!.toString().toLowerCase()} smooth={true} duration={600} spy={true} offset={-30} className="font-medium px-4 py-2 rounded hover:cursor-pointer hover:bg-gray-200 hover:text-black" activeClass="!bg-amber-300">{children}</Link>
+        <Link to={children!.toString().toLowerCase()} smooth={true} duration={600} spy={true} offset={-64} className="font-medium px-4 py-2 rounded hover:cursor-pointer hover:bg-gray-200 hover:text-black" activeClass="!bg-gray-700 !text-white" onClick={onClick}>{children}</Link>
     )
 }
--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,14 +1,22 @@
+import {useState} from "react";
+import { CgMenu } from "react-icons/cg";
 import NavButton from "./NavButton"
+
 export default function NavigationBar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    }
     const navItems = ["Home", "About", "Skills", "Experience", "Projects", "Contact"];
   return (
     <>
-    <nav className="fixed w-full top-0 flex px-20 py-4 bg-transparent text-black justify-between items-center border-b border-gray-300">
+    <nav className="fixed w-full top-0 flex px-8 md:px-20 h-20 bg-white text-black justify-between items-center border-b border-gray-300">
         <div>
             <h1 className="text-3xl font-bold">Z_</h1>
         </div>
         <div>
-            <ul className="flex space-x-10">
+            <ul className="hidden md:flex space-x-10">
                 {navItems.map((item) => (
                     <li key={item}>
                         <NavButton>{item}</NavButton>
@@ -16,7 +24,17 @@ export default function NavigationBar() {
                 ))}
             </ul>
         </div>
-        
+        <button onClick={toggleMenu}className="md:hidden hover:cursor-pointer hover:bg-gray-700 rounded px-3 py-2 hover:text-white">
+            <CgMenu size={30}/>
+        </button>
+
+        {isOpen && (
+            <div className="absolute top-18 left-0 w-full bg-white md:hidden flex flex-col items-center py-4 space-y-4 z-10">
+                {navItems.map((item) => (
+                    <NavButton onClick={toggleMenu} key={item}>{item}</NavButton>
+                ))}
+            </div>
+        )}
     </nav>
     </>
     
